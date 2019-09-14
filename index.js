2d67fa8510215b81b2e576df34a5f50170ed6a2b
Israel Oliveira <israel@radek.com.br>
2019-09-14 14:23:21 -0300

    Update .gitignore

--- index.js
+++ index.js
@@ -3,5 +3,17 @@ const fs = require('fs');
 
 const pdf = new PDFKit();
 
+// header
+pdf.image('image.png', 50, 50, { width: 50 })
+    .text('Company', 120, 50, { align: 'right' })
+    .text('Address, 0', 200, 70, { align: 'right' })
+    .text('City - State - Country', 200, 85, { align: 'right' });
+
+// main
+
+// footer
+pdf.fontSize(12)
+    .text('Company Name', 50, 695, { align: 'center' });
+
 pdf.pipe(fs.createWriteStream('file.pdf'));
 pdf.end();
